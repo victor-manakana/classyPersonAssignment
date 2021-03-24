f6f8196e234044450cc88de7e0dc940017875288
25779613 <duk.sal01@gmail.com>
2021-03-24 17:00:27 +0200

    Added variables

--- Contact_Management_Talent_Centric_Team1/src/contact_management_talent_centric_team1/Contact_Management_Talent_Centric_Team1.java
+++ Contact_Management_Talent_Centric_Team1/src/contact_management_talent_centric_team1/Contact_Management_Talent_Centric_Team1.java
@@ -9,15 +9,36 @@ package contact_management_talent_centric_team1;
  *
  * @author SALMAN
  */
+
+
 public class Contact_Management_Talent_Centric_Team1
 {
 
     /**
      * @param args the command line arguments
      */
+    static String FirstName;
+    static String MiddelName;
+    static String LastName;
+    static String Gender;
+    static String DateOfBirth;
+    static String Country;
+    static String City;
+    static String StreetAddress;
+    static String MobileNo;
+    static String HomeNo;
+    static String OfficeNo;
+    static String PersonalEmail;
+    static String WorkEmail;
+            
+    
     public static void main(String[] args)
     {
         // TODO code application logic here
     }
     
+    public void ContactQuestions()
+    {
+        
+    }
 }
