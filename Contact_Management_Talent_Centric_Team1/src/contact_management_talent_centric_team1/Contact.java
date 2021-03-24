10bd6feb9fee6fdc4ec9073bd04fbf4ca3ad2fc8
25779613 <duk.sal01@gmail.com>
2021-03-24 16:27:57 +0200

    Initial commit

---
+++ Contact_Management_Talent_Centric_Team1/src/contact_management_talent_centric_team1/Contact.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contact_management_talent_centric_team1;
+
+/**
+ *
+ * @author SALMAN
+ */
+public class Contact
+{
+    
+}
